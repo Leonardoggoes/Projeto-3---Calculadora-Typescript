3f4895dda3de4894f277072ae3598ef65cd897a8
Leonardoggoes <127688040+Leonardoggoes@users.noreply.github.com>
2023-04-06 16:41:05 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Projeto-3---Calculadora-Typescript
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-6hkftk)
---
+++ index.ts
@@ -0,0 +1,42 @@
+import './style.css';
+
+const myButtons = document.querySelectorAll('.botao')
+const botaoAc = document.querySelector('.botao-allclean')
+const botaoC = document.querySelector('.botao-clean')
+const botaoEqual = document.querySelector('.botao-equal')
+let telaCalculadora = document.getElementById('tela-calculadora')
+
+// Adicionar um ouvinte de evento de clique a cada botão
+myButtons.forEach((button) => {
+  button.addEventListener('click', handleClick);
+});
+
+botaoAc.addEventListener('click', cleanAll);
+botaoC.addEventListener('click', clean);
+botaoEqual.addEventListener('click', calcular);
+
+// Definir a função de tratamento de eventos
+function handleClick(event) {
+    const valorBotao = event.target.textContent;
+    verificarTamanhoMax();
+    telaCalculadora.textContent += valorBotao;}
+
+
+function cleanAll(event){
+  telaCalculadora.textContent = "";}
+
+function clean(event){
+  telaCalculadora.textContent = telaCalculadora.innerHTML.substring(0, telaCalculadora.innerHTML.length -1);}
+
+function calcular(){
+  if (telaCalculadora.innerHTML != ""){
+    const expressao = telaCalculadora.innerHTML.replace(/X/g, "*");
+    telaCalculadora.textContent = eval(expressao);
+  }}
+
+function verificarTamanhoMax(){
+  if(telaCalculadora.innerHTML.length == 12){
+    console.log("cheio");
+    return true;
+  }
+}
