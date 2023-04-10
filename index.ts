661af112c6554ea31c18476294922ea6c8e0017e
Leonardoggoes <127688040+Leonardoggoes@users.noreply.github.com>
2023-04-10 15:59:14 -0300

    3 files modified

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
 import './style.css';
 
-const myButtons = document.querySelectorAll('.botao')
-const botaoAc = document.querySelector('.botao-allclean')
-const botaoC = document.querySelector('.botao-clean')
-const botaoEqual = document.querySelector('.botao-equal')
-let telaCalculadora = document.getElementById('tela-calculadora')
+const myButtons = document.querySelectorAll('.botao');
+const botaoAc = document.querySelector('.botao-allclean');
+const botaoC = document.querySelector('.botao-clean');
+const botaoEqual = document.querySelector('.botao-equal');
+let telaCalculadora = document.getElementById('tela-calculadora');
 
 myButtons.forEach((button) => {
   button.addEventListener('click', handleClick);
@@ -14,28 +14,27 @@ botaoAc.addEventListener('click', cleanAll);
 botaoC.addEventListener('click', clean);
 botaoEqual.addEventListener('click', calcular);
 
-
 function handleClick(event) {
     const valorBotao = event.target.textContent;
-    verificarTamanhoMax();
-    telaCalculadora.textContent += valorBotao;}
+    telaCalculadora.textContent += valorBotao;
+}
 
+function cleanAll(event) {
+  telaCalculadora.textContent = '';
+}
 
-function cleanAll(event){
-  telaCalculadora.textContent = "";}
+function clean(event) {
+  telaCalculadora.textContent = telaCalculadora.innerHTML.substring(
+    0,
+    telaCalculadora.innerHTML.length - 1
+  );
+}
 
-function clean(event){
-  telaCalculadora.textContent = telaCalculadora.innerHTML.substring(0, telaCalculadora.innerHTML.length -1);}
-
-function calcular(){
-  if (telaCalculadora.innerHTML != ""){
-    const expressao = telaCalculadora.innerHTML.replace(/X/g, "*");
+function calcular() {
+  if (telaCalculadora.innerHTML != '') {
+    const expressao = telaCalculadora.innerHTML.replace('X', '*');
     telaCalculadora.textContent = eval(expressao);
-  }}
-
-function verificarTamanhoMax(){
-  if(telaCalculadora.innerHTML.length == 12){
-    console.log("cheio");
-    return true;
   }
 }
+
+
